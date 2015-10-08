d7c502b034dc0648c8acc199529d4c4ab30466da
Daniel Rodriguez <daniel@danielrodriguez.name>
2015-10-08 13:22:51 -0400

    Create RestKitCocoaTouchFramework

---
+++ RestKitCocoaTouchFramework/RestKitCocoaTouchFramework.h
@@ -0,0 +1,19 @@
+//
+//  RestKitCocoaTouchFramework.h
+//  RestKitCocoaTouchFramework
+//
+//  Created by Daniel Rodriguez on 10/8/15.
+//  Copyright © 2015 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RestKitCocoaTouchFramework.
+FOUNDATION_EXPORT double RestKitCocoaTouchFrameworkVersionNumber;
+
+//! Project version string for RestKitCocoaTouchFramework.
+FOUNDATION_EXPORT const unsigned char RestKitCocoaTouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RestKitCocoaTouchFramework/PublicHeader.h>
+
+
